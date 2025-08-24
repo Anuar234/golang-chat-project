702c6cf5e19f0e984e23bba8f724d13837e13223
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-08-24 17:13:24 +0500

    added basic components for App

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,2 +1,11 @@
 import React from "react";
 import './Header.scss'
+
+
+const Header = () =>(
+    <div className="header">
+        <h2>Go and React Chat App</h2>
+    </div>
+);
+
+export default Header;
