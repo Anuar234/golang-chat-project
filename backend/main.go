175257bef120b90f1a7d267b0441a55a5a2512cf
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-08-31 01:08:59 +0500

    lkasdjf

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,23 @@
-package backend
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Anuar234/golang-chat-project/pkg/websocket"
+)
+
+func setupRoutes() {
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Anuar full stack chat project")
+	setupRoutes()
+	http.ListenAndServe(":9000", nil)
+}
