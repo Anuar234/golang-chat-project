5449385bbff2dd1a38399c324dc71727e871ae8b
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-09-02 18:17:48 +0500

    back developed

--- backend/pkg/websocket/pool.go
+++ backend/pkg/websocket/pool.go
@@ -1,12 +1,52 @@
 package websocket
 
+import "fmt"
 
-type Pool struct{
-	Register chan *Client
+type Pool struct {
+	Register   chan *Client
 	Unregister chan *Client
-	Clients map[*Client]bool
-	Broadcast chan Message
+	Clients    map[*Client]bool
+	Broadcast  chan Message
 }
 
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
 
-NewPool()
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Printf("Size of connection pool: %d\n", len(pool.Clients))
+			for client := range pool.Clients {
+				fmt.Println("Client:", client)
+				if err := client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."}); err != nil {
+					fmt.Println("Error sending join message:", err)
+				}
+			}
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Printf("Size of connection pool: %d\n", len(pool.Clients))
+			for client := range pool.Clients {
+				fmt.Println("Client:", client)
+				if err := client.Conn.WriteJSON(Message{Type: 1, Body: "User disconnected..."}); err != nil {
+					fmt.Println("Error sending disconnect message:", err)
+				}
+			}
+		case message := <-pool.Broadcast:
+			fmt.Println("Sending message to all clients")
+			for client := range pool.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					fmt.Println("Error broadcasting message:", err)
+					// Не возвращаемся, продолжаем отправку другим клиентам
+				}
+			}
+		}
+	}
+}
